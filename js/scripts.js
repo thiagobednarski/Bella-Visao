01fb33b25138ceb93213a403409327073df6580b
Thiago Bednarski <thiagoromano1985@gmail.com>
2025-07-27 16:13:04 -0300

    alterações js

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ toggleButton.addEventListener('click', () => {
 // MENU 768PX
 
 let slideIndex = 0;
-const slides = document.querySelectorAll(".carousel .slide");
+
 
 function showNextSlide() {
   slides[slideIndex].classList.remove("active");
@@ -33,3 +33,58 @@ setInterval(showNextSlide, 4000);
   });
 
   // video 
+
+  let currentSlide = 0;
+  const slides = document.querySelectorAll(".slide");
+  const totalSlides = slides.length;
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.classList.remove("active");
+      if (i === index) {
+        slide.classList.add("active");
+      }
+    });
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+  }
+
+  
+  setInterval(nextSlide, 5000);
+
+  
+  showSlide(currentSlide);
+
+//  banner 
+
+const form = document.getElementById("form-agendamento");
+  const mensagem = document.getElementById("mensagem-confirmacao");
+  const oculosAnimado = document.querySelector(".oculos-animado");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    const response = await fetch(form.action, {
+      method: "POST",
+      body: formData,
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    if (response.ok) {
+      form.reset();
+      mensagem.style.display = "block";
+      oculosAnimado.style.animation = "none"; 
+      void oculosAnimado.offsetWidth;         
+      oculosAnimado.style.animation = "flutuar 1.5s ease-out forwards";
+    } else {
+      alert("Erro ao enviar. Tente novamente.");
+    }
+  });
+
+  // agendamento
